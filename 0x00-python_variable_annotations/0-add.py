12c42857308644f87bf33105f53c0285d309d388
Alvin <alvinvaati@gmail.com>
2023-08-13 17:24:28 +0300

    format

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -6,7 +6,8 @@ adds a and b
 
 def add(a: float, b: float) -> float:
     """
-    takes two float numbers a and b as arguments and returns their sum as a float.
+    takes two float numbers a and b as
+    arguments and returns their sum as a float.
     """
     sum: float = a + b
     return sum
