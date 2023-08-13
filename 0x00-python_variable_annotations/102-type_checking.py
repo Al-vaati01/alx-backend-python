a45b640b0b5fda9aea82f0f0c04d4e4a5a5bb759
Alvin <alvinvaati@gmail.com>
2023-08-13 21:05:09 +0300

    type check

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -5,19 +5,25 @@ type-annotated function zoom_array that takes a tuple
 from typing import Tuple
 
 
-def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
+def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> Tuple[int, ...]:
     """
-    type-annotated function zoom_array that takes a tuple
+    A function that zooms in on the input tuple by repeating
+    each element a specified number of times.
+
+    Args:
+        lst (Tuple[int, ...]): The input tuple of integers.
+        factor (int, optional): The zoom factor. Default is 2.
+
+    Returns:
+        Tuple[int, ...]: The zoomed-in tuple.
     """
-    zoomed_in: Tuple = [
+    zoomed_in: Tuple[int, ...] = tuple(
         item for item in lst
         for i in range(factor)
-    ]
+    )
     return zoomed_in
 
 
-array = [12, 72, 91]
-
-zoom_2x = zoom_array(array)
-
-zoom_3x = zoom_array(array, 3.0)
+array: Tuple[int, ...] = (12, 72, 91)
+zoom_2x: Tuple[int, ...] = zoom_array(array)
+zoom_3x: Tuple[int, ...] = zoom_array(array, 3)
