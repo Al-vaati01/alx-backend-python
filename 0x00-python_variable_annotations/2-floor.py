201c116c252f3f3f5dde28491ee4857dea7c5d9f
Alvin <alvinvaati@gmail.com>
2023-08-13 19:50:50 +0300

    format

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
-import math
+
 """
 A function to calculate the floor value of a float number.
 """
+
+import math
 
 
 def floor(n: float) -> int:
     """
     floor: A function to calculate the floor value of a float number.
+
+    Args:
+        n (float): The input float number.
+
+    Returns:
+        int: The floor value of the input number.
     """
     fl: int = math.floor(n)
     return fl
