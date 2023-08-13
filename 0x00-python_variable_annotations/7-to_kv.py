ee942d0679fded1db920e43033f5f2cb3fe53497
Alvin <alvinvaati@gmail.com>
2023-08-13 20:03:44 +0300

    int/float to tuple

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-from typing import Union, Tuple
 """
 a type-annotated function to_kv that takes a string k and an int OR
 float v as arguments and returns a tuple. The first element of the tuple is
@@ -7,10 +6,14 @@ the string k.
 The second element is the square of the int/float v and should be annotated
 as a float.
 """
+from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[float, int]) -> Tuple[str, float]:
-    sqr: int | float
+    """
+    takes a string k and an int OR float v as arguments and returns a tuple.
+    """
+    sqr: Union[float, int] = 0
     sqr = v**2
     t: tuple[str, int] = (k, sqr)
     return t
