6148690f23f3db708e2ada6c22027d6287061320
Alvin <alvinvaati@gmail.com>
2023-08-13 17:21:29 +0300

    format

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 """
-type-annotated function add that takes a float a
+takes a float a
 and a float b as arguments and returns their sum as a float.
 """
 
 
 def add(a: int, b: int) -> float:
+    """
+    takes a float a
+    and a float b as arguments and returns their sum as a float.
+    """
     sum: float = a + b
     return sum
