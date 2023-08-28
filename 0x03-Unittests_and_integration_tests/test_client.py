088396dfafc32a7710a367f727a66b35bf9ec88d
Alvin <alvinvaati@gmail.com>
2023-08-28 15:34:31 +0300

    types

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,29 +1,30 @@
 #!/usr/bin/env python3
-"""A github org client"""
+"""A GitHub org client"""
+
 import unittest
 from parameterized import parameterized
 from unittest.mock import patch, PropertyMock
-
 from requests import HTTPError
 from client import GithubOrgClient
+from typing import Any, Dict
 
 
 class TestGithubOrgClient(unittest.TestCase):
     """TestGithubOrgClient"""
 
     @parameterized.expand([
-        ("google"),
-        ("abc"),
+        ("google",),
+        ("abc",),
     ])
     @patch('client.get_json')
-    def test_org(self, org_name, mock_get_json):
+    def test_org(self, org_name: str, mock_get_json: Any) -> None:
         """Test that GithubOrgClient.org returns the correct value"""
         test_class = GithubOrgClient(org_name)
         test_class.org()
         mock_get_json.assert_called_once_with(
             "https://api.github.com/orgs/{}".format(org_name))
 
-    def test_public_repos_url(self):
+    def test_public_repos_url(self) -> None:
         """Test that the result of _public_repos_url is the expected one
         based on the mocked payload
         """
@@ -36,7 +37,7 @@ class TestGithubOrgClient(unittest.TestCase):
                              payload["repos_url"])
 
     @patch('client.get_json')
-    def test_public_repos(self, mock_get_json):
+    def test_public_repos(self, mock_get_json: Any) -> None:
         """Test that the list of repos is what you expect from the chosen
         payload.
         Test that the mocked property and the mocked get_json was called once
@@ -54,7 +55,8 @@ class TestGithubOrgClient(unittest.TestCase):
         ({'license': {'key': 'my_license'}}, True),
         ({'license': {'key': 'other_license'}}, True),
     ])
-    def test_has_license(self, repo, expected_return):
+    def test_has_license(self, repo: Dict[str, Any],
+                         expected_return: bool) -> None:
         """Test that the result of has_license is the expected one based on
         the mocked payload.
         Test that the mocked get_json was called once
@@ -66,21 +68,19 @@ class TestGithubOrgClient(unittest.TestCase):
 class TestIntegrationGithubOrgClient(unittest.TestCase):
     """TestGithubOrgClient"""
     @parameterized.expand([
-        ("org_payload"),
-        ("repo_payload"),
-        ("expected_repos"),
-        ("apache2_repos"),
+        ("org_payload",),
+        ("repo_payload",),
+        ("expected_repos",),
+        ("apache2_repos",),
     ])
-    # setupClass
-    @classmethod
-    def setUpClass(cls):
+    @classmethod  # setupClass
+    def setUpClass(cls) -> None:
         """set up class"""
         cls.get_patcher = patch('requests.get', side_effect=HTTPError)
         cls.get_patcher.start()
 
-    # tearDownClass
-    @classmethod
-    def tearDownClass(cls):
+    @classmethod  # tearDownClass
+    def tearDownClass(cls) -> None:
         """tear down class"""
         cls.get_patcher.stop()
 
